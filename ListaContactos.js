c22c5b4b4ab13d24933a24b941f88cbf926212a8
Juan Carlos Camacho H <130729402+camachocodecrafter@users.noreply.github.com>
2023-06-12 15:40:28 -0600

    entregable 2 lista de contactos

--- ListaContactos.js
+++ ListaContactos.js
@@ -1,15 +1,26 @@
-let listaContactos = ["Juan Perez", "Maria Lopez", "Carlos Ramirez"];
+let listaContactos = [];
 
 function agregarContacto(contacto) {
   listaContactos.push(contacto);
   console.log("Contacto añadido:", contacto);
 }
 
-function borrarContacto(contacto) {
-  let indice = listaContactos.indexOf(contacto);
+function borrarContacto(id) {
+  let indice = -1;
+  for (let i = 0; i < listaContactos.length; i++) {
+    if (listaContactos[i].id === id) {
+      indice = i;
+      break;
+    }
+  }
+
   if (indice !== -1) {
-    listaContactos.splice(indice, 1);
-    console.log("Contacto borrado:", contacto);
+    let contactoBorrado = listaContactos.splice(indice, 1);
+    console.log(
+      "Contacto borrado:",
+      contactoBorrado[0].nombres,
+      contactoBorrado[0].apellidos
+    );
   } else {
     console.log("El contacto no existe en la lista.");
   }
@@ -18,6 +29,21 @@ function borrarContacto(contacto) {
 function imprimirContactos() {
   console.log("Lista de contactos:");
   for (let contacto of listaContactos) {
-    console.log(contacto);
+    console.log("ID:", contacto.id);
+    console.log("Nombre completo:", contacto.nombres, contacto.apellidos);
+    console.log("Teléfono:", contacto.telefono);
+    console.log("Ciudad:", contacto.ciudad);
+    console.log("Dirección:", contacto.direccion);
   }
 }
+
+let nuevoContacto = {
+  id: 1,
+  nombres: "Juan Carlos",
+  apellidos: "Camacho",
+  telefono: "5555-5555",
+  ciudad: "San Pedro Sula",
+  direccion: "Calle Principal",
+};
+
+agregarContacto(nuevoContacto);
